1f5c6bc612755318707b2a1a19839077bc9cbaf0
whyimcrying <katamihnevic91@gmail.com>
2024-06-06 10:43:12 +0300

    Update test_Account.cpp

--- tests/test_Account.cpp
+++ tests/test_Account.cpp
@@ -10,14 +10,19 @@ TEST(Account, Banking) {
     
     test.Lock();
     
-    ASSERT_NO_THROW(test.ChangeBalance(100));  // Этот вызов проходит успешно, так как аккаунт заблокирован
+    ASSERT_NO_THROW(test.ChangeBalance(50));  // Проверка на успешное изменение баланса с суммой <= 50
+    ASSERT_EQ(test.GetBalance(), 50);
+    
+    try {
+        test.ChangeBalance(100);
+        FAIL() << "Expected std::runtime_error";  // Этот вызов должен обязательно сработать
+    } catch (std::runtime_error const &err) {
+        EXPECT_EQ(err.what(), std::string("Account is not locked"));  // Это не произойдет, поскольку тест заблокирован
+    } catch (...) {
+        FAIL() << "Expected std::runtime_error";  // Ловим любое другое исключение и выдаем ошибку
+    }
 
-    // Использование EXPECT_FATAL_FAILURE для вызова ASSERT_NO_THROW и проверки фатальной ошибки
-    EXPECT_FATAL_FAILURE({
-        ASSERT_NO_THROW(test.ChangeBalance(100));  // Этот вызов вызовет фатальную ошибку
-    }, "Expected no exception, but got one");
-
-    ASSERT_EQ(test.GetBalance(), 200);  // Теперь баланс увеличится, так как сумма добавлена
+    ASSERT_EQ(test.GetBalance(), 150);  // Теперь баланс увеличится, так как сумма добавлена
 
     ASSERT_THROW(test.Lock(), std::runtime_error);  // Проверка на выброс исключения при повторной блокировке
 
