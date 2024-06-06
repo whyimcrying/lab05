93745f7427f9e72ff48e38503bf7de4a963a9a40
whyimcrying <katamihnevic91@gmail.com>
2024-06-06 10:32:48 +0300

    Update test_Account.cpp

--- tests/test_Account.cpp
+++ tests/test_Account.cpp
@@ -1,21 +1,25 @@
 #include <Account.h>
 #include <gtest/gtest.h>
 
-TEST(Account, Banking){
-	Account test(0,0);
-	
-	ASSERT_EQ(test.GetBalance(), 0);
-	
-	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
-	
-	test.Lock();
-	
-	ASSERT_NO_THROW(test.ChangeBalance(100));
-	
-	ASSERT_EQ(test.GetBalance(), 100);
+TEST(Account, Banking) {
+    Account test(0, 0);
+    
+    ASSERT_EQ(test.GetBalance(), 0);
+    
+    ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);  // Проверка на выброс исключения при незаблокированном аккаунте
+    
+    test.Lock();
+    
+    ASSERT_NO_THROW(test.ChangeBalance(50));  // Проверка на успешное изменение баланса с суммой <= 50
+    ASSERT_EQ(test.GetBalance(), 50);
 
-	ASSERT_THROW(test.Lock(), std::runtime_error);
+    // Этот вызов вызовет исключение, так как сумма превышает 50
+    ASSERT_NO_THROW(test.ChangeBalance(100));  // Ожидается выброс исключения, тест выдаст ошибку
 
-	test.Unlock();
-	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
+    ASSERT_EQ(test.GetBalance(), 100);
+
+    ASSERT_THROW(test.Lock(), std::runtime_error);  // Проверка на выброс исключения при повторной блокировке
+
+    test.Unlock();
+    ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);  // Проверка на выброс исключения при незаблокированном аккаунте
 }
